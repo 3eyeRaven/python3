19f8b08bce10d59e5335eafaf22666cf8ced2514
TankTank0816 <vprodigys@gmail.com>
2018-04-24 06:05:40 -0400

    Different types of variables. Variables are case-sensitive.

---
+++ variables.py
@@ -0,0 +1,20 @@
+# Variables hold different values.
+
+height = 332    #An integer
+
+weight = 332.9  #Floating point
+
+height = 'Really Tall'  #A String
+
+name = 'Jake'
+
+Name = 'Bobby'
+
+NAME = 'Jax'
+
+print name
+
+print height
+
+print weight
+
